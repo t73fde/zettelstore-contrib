b8f38b64475890d365da31df07cb2e42b8829942
stern <ds@zettelstore.de>
2024-02-26 11:57:53 +0000

    Presenter: update license
    FossilOrigin-Name: 1add34347813568f924c5c0f2c1df9453e5b2e48fc945700dd5100106e752c29

--- presenter/htmlenc.go
+++ presenter/htmlenc.go
@@ -1,11 +1,11 @@
 //-----------------------------------------------------------------------------
 // Copyright (c) 2022-present Detlef Stern
 //
-// This file is part of zettelstore slides application.
+// This file is part of Zettel Presenter.
 //
-// Zettelstore slides application is licensed under the latest version of the
-// EUPL (European Union Public License). Please see file LICENSE.txt for your
-// rights and obligations under this license.
+// Zettel Presenter is licensed under the latest version of the EUPL (European
+// Union Public License). Please see file LICENSE.txt for your rights and
+// obligations under this license.
 //
 // SPDX-License-Identifier: EUPL-1.2
 // SPDX-FileCopyrightText: 2022-present Detlef Stern
