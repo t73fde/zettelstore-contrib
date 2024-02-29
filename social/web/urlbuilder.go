aeaf3c6c07794d8343e7f6b2659b3a91634558b1
stern <ds@zettelstore.de>
2024-02-29 15:21:19 +0000

    Social: add web/URLBuilder
    FossilOrigin-Name: f989f57d952bf1ff78184ef8092a7fa2a67c89e0fa7745107e19cf1aba04b9f7

---
+++ social/web/urlbuilder.go
@@ -0,0 +1,84 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2024-present Detlef Stern
+//
+// This file is part of Zettel Social
+//
+// Zettel Social is licensed under the latest version of the EUPL (European
+// Union Public License). Please see file LICENSE.txt for your rights and
+// obligations under this license.
+//
+// SPDX-License-Identifier: EUPL-1.2
+// SPDX-FileCopyrightText: 2024-present Detlef Stern
+//-----------------------------------------------------------------------------
+
+package web
+
+import (
+	"net/url"
+	"strings"
+)
+
+// URLBuilder helps to build URLs
+type URLBuilder struct {
+	prefix   string
+	path     []string
+	fragment string
+	query    []urlQuery
+}
+type urlQuery struct{ key, val string }
+
+// NewURLBuilder creates a new URL builder with the fiven prefix.
+func NewURLBuilder(prefix string) *URLBuilder {
+	return &URLBuilder{prefix: prefix}
+}
+
+// AddPath adds a new path element.
+func (ub *URLBuilder) AddPath(p string) *URLBuilder {
+	for len(p) > 0 && p[0] == '/' {
+		p = p[1:]
+	}
+	if p != "" {
+		ub.path = append(ub.path, p)
+	}
+	return ub
+}
+
+// SetFragment stores the fragment
+func (ub *URLBuilder) SetFragment(frag string) *URLBuilder {
+	ub.fragment = frag
+	return ub
+}
+
+// AddQuery adds a new key/value query parameter
+func (ub *URLBuilder) AddQuery(key, value string) *URLBuilder {
+	ub.query = append(ub.query, urlQuery{key, value})
+	return ub
+}
+
+// String constructs a string representation of the URL.
+func (ub *URLBuilder) String() string {
+	var sb strings.Builder
+
+	sb.WriteString(ub.prefix)
+	for _, p := range ub.path {
+		sb.WriteByte('/')
+		sb.WriteString(url.PathEscape(p))
+	}
+	if ub.fragment != "" {
+		sb.WriteByte('#')
+		sb.WriteString(ub.fragment)
+	}
+	for i, q := range ub.query {
+		if i == 0 {
+			sb.WriteByte('?')
+		} else {
+			sb.WriteByte('&')
+		}
+		sb.WriteString(q.key)
+		if val := q.val; val != "" {
+			sb.WriteByte('=')
+			sb.WriteString(url.QueryEscape(val))
+		}
+	}
+	return sb.String()
+}
